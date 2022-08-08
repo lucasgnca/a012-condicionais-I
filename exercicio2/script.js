5f6de73889e690673054954a02958303cdf1bfba
Lucas <lc.lucas070@gmail.com>
2022-08-08 12:51:53 -0300

    Resolucao exercicios A012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 17;
+let terminouMedio = false;
+let isCursandoOutraFaculdade = false;
 
-if(){
-
+if(idade >= 18){
+    console.log('Tem 18 anos ou mais.')
 }else{
-	
+	console.log('Tem menos de 18 anos')
+}
+
+if (terminouMedio === true){
+    console.log('É maior de idade')
+} else{
+    console.log('É menor de idade')
+}
+
+if (isCursandoOutraFaculdade === true){
+    console.log('Está cursando outra faculdade')
+} else{
+    console.log('Não está cursando outra faculdade')
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Qual a sua nacionalidade?') //= adicione o prompt aqui
+
+const nacionalidades = ['brasileiro', 'argentino', 'uruguaio', 'chileno', 'colombiano']
+
+if(nacionalidade === 'brasileiro' ||
+   nacionalidade === 'argentino' ||
+   nacionalidade === 'uruguaio' ||
+   nacionalidade === 'chileno' ||
+   nacionalidade === 'colombiano') {
+    console.log(`Sua nacionalidade é: ${nacionalidade}.`)
+}
